7fe0866bde03c56d100cdc02b6acd2b10b22411b
Aman Kumar Dubey <111529518+amandubeyin@users.noreply.github.com>
2024-01-14 16:38:54 +0000

    CheckTheNumber

--- projects/guessTheNumber.js
+++ projects/guessTheNumber.js
@@ -1,18 +1,25 @@
 let gameNum = Math.round(Math.random()*100); //Create a random number
 function guessTheNumber(){ // Create a function 
     let userNum = prompt("Guess The Number(1-100): "); // input from user
-    if(userNum == gameNum){
-        console.log("Yes, The Number is",userNum);
-        alert("Yes, The Number is",userNum);
-    }
-    else if(userNum>gameNum){
-        console.log("Wrong !! Number is Smaller then",userNum);
-        alert("Wrong !! Number is Smaller then",userNum);
-        guessTheNumber()
+    if(userNum>=1 && userNum<=100){                    // Check The Number
+        if(userNum == gameNum){
+            console.log("Yes, The Number is",userNum);
+            alert("Yes, The Number is",userNum);
+        }
+        else if(userNum>gameNum){
+            console.log("Wrong !! Number is Smaller then",userNum);
+            alert("Wrong !! Number is Smaller then",userNum);
+            guessTheNumber()
+        }
+        else{
+            console.log("wrong !! Number is Grater then",userNum);
+            alert("Wrong !! Number is Grater then",userNum);
+            guessTheNumber()
+        }
     }
     else{
-        console.log("wrong !! Number is Grater then",userNum);
-        alert("Wrong !! Number is Grater then",userNum);
+        console.log("The entered number should be between 1 and 100.");
+        alert("The entered number should be between 1 and 100.");
         guessTheNumber()
     }
 }
